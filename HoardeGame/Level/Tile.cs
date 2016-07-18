e320b8ce3579ff3ebcdef095d9d055c8004130dd
Huskitch <huskitch@gmail.com>
2016-07-18 12:04:53 +0100

    Started on a tile generator and a tile class

---
+++ HoardeGame/Level/Tile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HoardeGame.Level
+{
+    public class Tile
+    {
+        private Vector2 Position;
+        private Vector2 Scale;
+        private Texture2D Texture;
+
+        public Tile(Vector2 pos, Vector2 scale, Texture2D texture)
+        {
+            Position = pos;
+            Scale = scale;
+            Texture = texture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, new Rectangle((int)Position.X, (int)Position.Y, (int)Scale.X, (int)Scale.Y), null, Color.White, 0f,
+                Vector2.Zero, SpriteEffects.None, 0f);
+        }
+    }
+}
