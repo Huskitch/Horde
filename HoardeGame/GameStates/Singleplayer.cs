a0a0ae21a3c81593d5a71a0eb2782a0a56fd5d68
Michal Manda <gmoder.virusek20@gmail.com>
2016-07-18 15:57:42 +0200

    Added progress bar

--- HoardeGame/GameStates/Singleplayer.cs
+++ HoardeGame/GameStates/Singleplayer.cs
@@ -13,17 +13,16 @@ namespace HoardeGame.GameStates
         private SpriteBatch spriteBatch;
         private GraphicsDevice graphicsDevice;
 
-        private Label _helloWorldLabel;
-        private Button _helloWorldButton;
-
         private DungeonLevel dungeon;
+
+        private ProgressBar _bar;
 
         public SinglePlayer(ContentManager content, SpriteBatch batch, GraphicsDevice device, GameWindow window)
         {
             graphicsDevice = device;
             spriteBatch = batch;
 
-            ResourceManager.Init();
+            ResourceManager.Init(graphicsDevice);
             ResourceManager.LoadContent(content);
 
             GuiBase.Font = ResourceManager.Font("BasicFont");
@@ -34,13 +33,13 @@ namespace HoardeGame.GameStates
 
         public override void Start()
         {
-            _helloWorldLabel = new Label(this, nameof(_helloWorldLabel))
+            new Label(this, "helloWorldLabel")
             {
                 Position = new Vector2(10, 10),
                 Text = "Hello world!"
             };
 
-            _helloWorldButton = new Button(this, nameof(_helloWorldButton))
+            new Button(this, "helloWorldButton")
             {
                 Position = new Vector2(10, 30),
                 TargetRectangle = new Rectangle(10, 30, 128, 32),
@@ -54,11 +53,25 @@ namespace HoardeGame.GameStates
                     Environment.Exit(0);
                 }
             };
+
+            _bar = new ProgressBar(this, "helloWorldProgressBar")
+            {
+                Position = new Vector2(10, 70),
+                ProgressBarTexture = ResourceManager.Texture("BasicProgressBar"),
+                BarHeight = 23,
+                BarStart = new Vector2(1, 1),
+                BarWidth = 63,
+                TargetRectangle = new Rectangle(10, 70, 65, 25),
+                Color = Color.Blue,
+                Progress = 1f
+            };
         }
 
         public override void Update(GameTime gameTime)
         {
             MouseState state = Mouse.GetState();
+
+            _bar.Progress = gameTime.TotalGameTime.Milliseconds / 1000f;
 
             DoCheck(gameTime, new Point(state.X, state.Y), state.LeftButton == ButtonState.Pressed);
 
