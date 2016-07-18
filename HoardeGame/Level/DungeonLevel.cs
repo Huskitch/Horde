7921827b80e0efeb6d162e62515223369eec6af5
Huskitch <huskitch@gmail.com>
2016-07-18 12:37:32 +0100

    Started a dungeon class and added a resource manager to load the content at the first boot

---
+++ HoardeGame/Level/DungeonLevel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HoardeGame.Level
+{
+    public class DungeonLevel
+    {
+        private LevelGenerator levelGen;
+        private List<Tile> mapTiles;
+
+        private int[,] map;
+        private int mapWidth = 64;
+        private int mapHeight = 64;
+
+        public DungeonLevel()
+        {
+            
+        }
+
+        public void GenerateLevel(int width, int height)
+        {
+            
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
---
+++ HoardeGame/ResourceManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace HoardeGame
+{
+    public class ResourceManager
+    {
+        private static Dictionary<string, Texture2D> textures;
+        private static Dictionary<string, SpriteFont> fonts;
+        private static Dictionary<string, SoundEffect> sounds;
+        private static Dictionary<string, Song> songs;
+        private static GraphicsDevice device;
+
+        public static void Init()
+        {
+            textures = new Dictionary<string, Texture2D>();
+            fonts = new Dictionary<string, SpriteFont>();
+            sounds = new Dictionary<string, SoundEffect>();
+            songs = new Dictionary<string, Song>();
+        }
+
+        public static void LoadContent(ContentManager content)
+        {
+            LoadTextures(content);
+            LoadFonts(content);
+            LoadSounds(content);
+            LoadSongs(content);
+        }
+
+        public static void LoadTextures(ContentManager content)
+        {
+
+        }
+
+        public static void LoadFonts(ContentManager content)
+        {
+
+        }
+
+        public static void LoadSounds(ContentManager content)
+        {
+
+        }
+
+        public static void LoadSongs(ContentManager content)
+        {
+
+        }
+
+        public static Texture2D Texture(string key)
+        {
+            return textures[key];
+        }
+
+        public static SpriteFont Font(string key)
+        {
+            return fonts[key];
+        }
+
+        public static SoundEffect SoundEffect(string key)
+        {
+            return sounds[key];
+        }
+
+        public static Song Song(string key)
+        {
+            return songs[key];
+        }
+    }
+}
+
+
