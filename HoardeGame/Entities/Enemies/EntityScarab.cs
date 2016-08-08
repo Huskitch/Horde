f19e3c2d97820390fc23485f0306398462259bd0
Huskitch <huskitch@gmail.com>
2016-08-08 20:56:10 +0100

    Added scarab class

---
+++ HoardeGame/Entities/Enemies/EntityScarab.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using HoardeGame.Entities.Base;
+using HoardeGame.Gameplay.Gems;
+using HoardeGame.Gameplay.Level;
+using HoardeGame.Graphics;
+using HoardeGame.Resources;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HoardeGame.Entities.Enemies
+{
+    public class EntityScarab : EntityBaseEnemy
+    {
+        private readonly AnimatedSprite animator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HoardeGame.Entities.Enemies.EntityBat"/> class.
+        /// </summary>
+        /// <param name="level"><see cref="DungeonLevel"/> to place this entity in</param>
+        /// <param name="serviceContainer"><see cref="GameServiceContainer"/> for resolving DI</param>
+        public EntityScarab(DungeonLevel level, GameServiceContainer serviceContainer) : base(level, serviceContainer)
+        {
+            FixtureFactory.AttachCircle(ConvertUnits.ToSimUnits(10f), 1f, Body);
+            Body.CollisionCategories = Category.Cat3;
+            Body.CollidesWith = Category.All;
+            Body.BodyType = BodyType.Dynamic;
+            Body.LinearDamping = 20f;
+            Body.FixedRotation = true;
+
+            Damage = 1;
+            Health = 10;
+
+            GemDrop = new GemDropInfo
+            {
+                MinDrop =
+                {
+                    RedGems = 3,
+                    GreenGems = 2,
+                    BlueGems = 1,
+                    Keys = 0
+                },
+                MaxDrop =
+                {
+                    RedGems = 6,
+                    GreenGems = 4,
+                    BlueGems = 2,
+                    Keys = 0
+                }
+            };
+
+            animator = new AnimatedSprite(serviceContainer.GetService<IResourceProvider>().GetTexture("Scarab"), serviceContainer);
+            animator.AddAnimation("Flap", 32, 0, 1, 100);
+        }
+
+        /// <inheritdoc/>
+        public override void Update(GameTime gameTime)
+        {
+            UpdateAI(gameTime);
+            animator.Update(gameTime);
+        }
+
+        /// <inheritdoc/>
+        public override void Draw(EffectParameter parameter)
+        {
+            Vector2 screenPos = ConvertUnits.ToDisplayUnits(Position);
+            animator.DrawAnimation("Flap", screenPos, Color.White, parameter, CurrentBlinkFrame);
+        }
+    }
+}
